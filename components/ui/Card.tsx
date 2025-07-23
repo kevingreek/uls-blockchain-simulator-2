46d2d2dcee6b462b05779eb7d296428d73ec6e93
kevingreek <kevingreek@gmail.com>
2025-07-23 20:34:40 +0300

    Delete components/ui directory

--- components/ui/Card.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Card({ children, className = "" }: { children: React.ReactNode; className?: string }) {
-  return (
-    <div className={`rounded border border-gray-300 bg-white p-4 shadow-sm ${className}`}>
-      {children}
-    </div>
-  );
-}
